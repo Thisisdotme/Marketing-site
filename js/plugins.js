e4cc1ca8021062f2508d004c49a38f26d5a3455a
Cameron Drake <cdrake757@gmail.com>
2012-07-13 16:17:34 -0700

    Created Responsive Layouts for iPhone and iPad. Some fixesare still in order for Gingerbread, Safari, iPhone (horizontal) and potentially Internet Explorer. The layout is solid on iPad, FF, Chrome & iPhone portrait view.

--- js/plugins.js
+++ js/plugins.js
@@ -10,3 +10,57 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
+$(document).ready(function(){
+        $('#ticker').list_ticker({
+                speed:4000,
+		    effect:'fade'
+		    })              
+	    });
+/* List Ticker by Alex Fish 
+// www.alexefish.com
+//
+// options:
+//
+// effect: fade/slide
+// speed: milliseconds
+*/
+
+(function($){
+    $.fn.list_ticker = function(options){
+    
+	var defaults = {
+	    speed:4000,
+	    effect:'slide'
+	};
+    
+	var options = $.extend(defaults, options);
+    
+	return this.each(function(){
+      
+		var obj = $(this);
+		var list = obj.children();
+		list.not(':first').hide();
+      
+		setInterval(function(){
+        
+			list = obj.children();
+			list.not(':first').hide();
+        
+			var first_li = list.eq(0)
+			    var second_li = list.eq(1)
+			    
+			    if(options.effect == 'slide'){
+				first_li.slideUp();
+				second_li.slideDown(function(){
+					first_li.remove().appendTo(obj);
+				    });
+			    } else if(options.effect == 'fade'){
+				first_li.fadeOut(function(){
+					second_li.fadeIn();
+					first_li.remove().appendTo(obj);
+				    });
+			    }
+		    }, options.speed)
+		    });
+    };
+})(jQuery);
