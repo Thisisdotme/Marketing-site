11414384a0e09a68e220a50ba5e251d271e27126
Cameron Drake <cdrake757@gmail.com>
2012-07-25 15:10:12 -0700

    fixed directory structure

--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
+http_path = "www"
+css_dir = "www/css"
 sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+images_dir = "www/img"
+javascripts_dir = "www/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
